cd99fcda4c8d05c1651daeecd7fa594222d8ae7f
Sam POUZET <samuel.pouzet@yahoo.fr>
2021-11-20 12:56:54 +0100

    add migrations

---
+++ laminas-mvc-skeleton/data/doctrine/migrations/user/2021/Version20211120112049.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace User\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211120112049 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        $description = 'This is the initial migration for the user\'s tables';
+        return $description;
+    }
+
+    public function up(Schema $schema): void
+    {
+        //create table user
+        $table = $schema->createTable('user');
+        $table->addColumn('id', 'integer', ['autoincrement'=>true]);
+        $table->addColumn('name', 'text', ['notnull'=>true, 'lenght'=>128]);
+        $table->addColumn('login', 'text', ['notnull'=>true, 'lenght'=>128]);
+        $table->addColumn('firstname', 'text', ['notnull'=>true, 'lenght'=>128]);
+        $table->addColumn('email', 'integer', ['notnull'=>true]);
+        $table->addColumn('avatar', 'text', ['notnull'=>true, 'lenght'=>156]);
+        $table->addColumn('password', 'text', ['notnull'=>true, 'lenght'=>225]);
+        $table->addColumn('date_create', 'datetime', ['notnull'=>true]);
+        $table->addColumn('status', 'integer', ['notnull'=>true, 'lenght'=>2]);
+        $table->setPrimaryKey(['id']);
+        $table->addOption('engine' , 'InnoDB');
+
+        //create table user_role
+        $table = $schema->createTable('user_role');
+        $table->addColumn('id', 'integer', ['autoincrement'=>true]);
+        $table->addColumn('id_user', 'integer', ['notnull'=>true]);
+        $table->addColumn('id_role', 'integer', ['notnull'=>true]);
+        $table->setPrimaryKey(['id']);
+        $table->addOption('engine' , 'InnoDB');
+
+        //create table role
+        $table = $schema->createTable('role');
+        $table->addColumn('id', 'integer', ['autoincrement'=>true]);
+        $table->addColumn('name', 'string', ['notnull'=>true]);
+        $table->addColumn('is_active', 'boolean');
+        $table->setPrimaryKey(['id']);
+        $table->addOption('engine' , 'InnoDB');
+
+        //create table role_hierarchy
+        $table = $schema->createTable('role_hierarchy');
+        $table->addColumn('id', 'integer', ['autoincrement'=>true]);
+        $table->addColumn('id_parent', 'integer', ['notnull'=>true]);
+        $table->addColumn('id_child', 'integer', ['notnull'=>true]);
+        $table->setPrimaryKey(['id']);
+        $table->addOption('engine' , 'InnoDB');
+
+        //create table role_privilege
+        $table = $schema->createTable('role_privilege');
+        $table->addColumn('id', 'integer', ['autoincrement'=>true]);
+        $table->addColumn('id_role', 'integer', ['notnull'=>true]);
+        $table->addColumn('id_privilege', 'integer', ['notnull'=>true]);
+        $table->setPrimaryKey(['id']);
+        $table->addOption('engine' , 'InnoDB');
+
+        //create table privilege
+        $table = $schema->createTable('privilege');
+        $table->addColumn('id', 'integer', ['autoincrement'=>true]);
+        $table->addColumn('name', 'text', ['notnull'=>true, 'lenght'=>128]);
+        $table->addColumn('is_active', 'boolean');
+        $table->setPrimaryKey(['id']);
+        $table->addOption('engine' , 'InnoDB');
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->dropTable('user');
+        $schema->dropTable('user_role');
+        $schema->dropTable('role');
+        $schema->dropTable('role_hierarchy');
+        $schema->dropTable('role_privilege');
+        $schema->dropTable('privilege');
+    }
+}
